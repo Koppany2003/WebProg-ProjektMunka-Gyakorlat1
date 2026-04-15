9cdd49c1c90a6e6ef69e0a156a2f6278a265eb7e
Koppany2003 <kocsiskoppany1@gmail.com>
2026-04-15 20:01:41 +0200

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,27 @@
+<?php
+$ablakcim = array(
+    'cim' => 'GAMF Pizzéria.',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'logo',
+	'cim' => 'GAMF Pizzéria',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'GAMF Pizzéria Kft.'
+);
+
+$oldalak = array(
+    '/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
+    'pizzak' => array('fajl' => 'crud', 'szoveg' => 'Pizzák (CRUD)', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
