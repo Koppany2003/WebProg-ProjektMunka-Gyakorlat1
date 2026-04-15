f6ac310e2f3871f433fbfecccecf50ba8a587ceb
Döme Balázs Márk <165893349+dmebalzsHUN@users.noreply.github.com>
2026-04-15 20:54:08 +0200

    Add files via upload

---
+++ cimlap.tpl.php
@@ -0,0 +1,19 @@
+<h2>Üdvözöljük a GAMF Pizzériában!</h2>
+
+<p>Kecskemét legjobb pizzái, egyenesen a kampusz mellől! Friss alapanyagok, gyors kiszállítás.</p>
+
+<h3>Így készül a nálunk a pizza:</h3>
+<div style="margin-bottom: 20px;">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/1-SJGQ2HLp8" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen></iframe>
+</div>
+
+<hr>
+
+<h3>Itt találsz meg minket:</h3>
+<div style="margin-bottom: 20px;">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.331206138676!2d19.66440261560593!3d46.89612297914442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c501173%3A0x868c227429158fb6!2sNeumann%20J%C3%A1nos%20Egyetem%20GAMF%20M%C5%B1szaki%20%C3%A9s%20Informatikai%20Kar!5e0!3m2!1shu!2shu!4v1684000000000!5m2!1shu!2shu" 
+        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
